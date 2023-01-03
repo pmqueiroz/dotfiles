64a83bfcfb911947f7456766762437518a95d57a
pmqueiroz <pedroqueiroz593@gmail.com>
2023-01-02 22:29:12 -0300

    feat: improve commit function

--- helpers.sh
+++ helpers.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
-
-GREEN="\u001b[32m"
-MAGENTA="\u001b[35m"
-CYAN="\u001b[36m"
-YELLOW="\033[33m"
-RESET="\033[0m"
-RED="\033[0;31m"
+source settings/bash-commands
 
 declare -A options;
 
@@ -15,22 +9,6 @@ function load_options() {
          options[${opt:2}]=true
       fi
    done
-}
-
-function upper() {
-   echo $1 | tr '[:lower:]' '[:upper:]'
-}
-
-function log() {
-   declare -A levels=( ["info"]="$CYAN" ["error"]="$RED" ["action"]="$MAGENTA" ["ask"]="$YELLOW" )
-   log_level=$1
-
-   if [[ ${options[quiet]} == true ]] && [[ $log_level != "error" ]] ; then
-      return 125 # operation canceled code
-   fi
-
-   display_level=$(upper $log_level)
-   printf "${levels[$log_level]}[$display_level]$RESET ${@:2}\n\n"
 }
 
 function has_installed_sources_before() {
