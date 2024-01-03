ff2a8792989695fac8623d86e4d7180852c4737d
pmqueiroz <pedroqueiroz593@gmail.com>
2024-01-03 09:45:51 -0300

    feat: skip if installed pnpm

--- recipes/pnpm.sh
+++ recipes/pnpm.sh
@@ -2,7 +2,11 @@
 shopt -s expand_aliases
 
 function install_pnpm {
-   _ asdf plugin-add pnpm
-   _ asdf install pnpm latest
-   _ asdf global pnpm latest
+   if ! command -v pnpm &> /dev/null; then
+      _ asdf plugin-add pnpm
+      _ asdf install pnpm latest
+      _ asdf global pnpm latest
+   else 
+      log warn "pnpm already installed. skipping"
+   fi
 }
