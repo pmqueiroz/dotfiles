02cec36616955cdc2c9853c68f5a31e3bdd099fd
pmqueiroz <pedroqueiroz593@gmail.com>
2024-06-26 13:53:57 +0000

    update install

--- install.sh
+++ install.sh
@@ -1,6 +1,144 @@
-#!/usr/bin/env bash
 shopt -s expand_aliases
-source .dotfilesrc
+
+shopt -s expand_aliases
+
+alias glo='git log --oneline'
+alias repo='gh repo view --web'
+alias debug='gum log -s -t kitchen -l debug'
+alias gum_log='gum log -t kitchen -l'
+
+function pr {
+   gh pr view --json number -q '.number' &> /dev/null
+
+   if [ $? -ne 0 ]; then
+      title=$(commit_title $(git branch --show-current))
+      gh pr create --assignee @me --title "$title" --web $@
+      return 
+   fi
+
+   gh pr view --web
+}
+
+function commit_title {
+   echo $1 | awk '{gsub("/",": "); print}' | awk '{gsub("-"," "); print}'
+}
+
+function git_style {
+  gum style "$(gum style --foreground "#f14e32" '') $(gum style --bold --underline --foreground "#f14e32" "$@")"
+}
+
+function fpush {
+   if ! git rev-parse --is-inside-work-tree &> /dev/null; then
+      gum_log error "not a git repository"
+      return 128
+   fi
+
+   branch=`git branch --show-current`
+   gum_log info "pushing and setting upstream to $(git_style $branch)"
+
+   git push $1 --set-upstream origin $branch $2
+}
+
+function commit {
+   if ! git rev-parse --is-inside-work-tree &> /dev/null; then
+      gum_log error "not a git repository"
+      return 128
+   fi
+   declare -a commit_options;
+   declare -a inputted_message;
+
+   for arg in $@; do
+      if [[ $arg == -* ]]; then
+         commit_options+=( "$arg" )
+      else
+         inputted_message+=( "$arg" )
+      fi
+      shift
+   done
+
+   inputted_message="${inputted_message[@]}"
+
+   git add .
+
+   if test -z "$inputted_message"
+   then
+      commitmsg=`commit_title $(git branch --show-current)`
+   else
+      commitmsg="$inputted_message"
+   fi
+
+   gum_log info "commiting files with message $(git_style "$commitmsg")"
+
+   gum style --foreground 12 --margin "0 1" -- "$(git diff --name-only --cached)"
+
+   gum confirm "confirm?" && git commit -m "${commitmsg}" ${commit_options[@]} || {
+      gum_log error exiting...
+      git reset
+   }
+}
+
+function checkout {
+   if ! git rev-parse --is-inside-work-tree &> /dev/null; then
+      gum_log error "not a git repository"
+      return 128
+   fi
+
+   current_branch=$(git branch --show-current)
+
+   if test -z "$1"; then
+      branch_to_checkout=$(git symbolic-ref refs/remotes/origin/HEAD | sed 's@^refs/remotes/origin/@@')
+   else
+      branch_to_checkout="$1"
+   fi
+
+   if [ $current_branch != $branch_to_checkout ]; then
+      gum_log info "checking out to $(git_style $branch_to_checkout)"
+      git checkout --quiet $branch_to_checkout
+   fi
+
+   gum_log info 'updating branch'
+
+   git pull --rebase --quiet
+
+   local_branchs=$(git for-each-ref --format='%(refname:short)' refs/heads/ | sed /^$branch_to_checkout\$/d)
+
+   if test -n "$local_branchs"; then
+      gum_log info 'deleting other branches. was:'
+      for branch in ${local_branchs[@]}; do 
+         git_style $branch
+      done
+
+      echo $local_branchs | xargs git branch -q -D
+   fi
+}
+
+function run_node {
+   tmp_file=$(mktemp --suffix=.js)
+   code $tmp_file --wait
+   file_content=$(cat ${tmp_file})
+   node -p -e "${file_content}"
+   rm -rf $tmp_file
+}
+
+function checksum {
+   md5sum $1 | awk '{ print $1 }'
+}
+
+function hot {
+  if (( $# < 2 )); then
+   gum_log error 'USAGE: hot <command> <file1> [<file2> ... <fileN>]'
+  else
+   script=$1
+   shift
+   a='';
+
+   while true; do
+      b=`ls -l $*`
+      [[ $a != $b ]] && a=$b && eval $script;
+      sleep .5;
+   done
+  fi
+}
 
 if ! command -v brew &> /dev/null; then
    echo "[FATAL] brew is not installed. Please install Homebrew first."
@@ -27,11 +165,110 @@ if ! command -v md5sum &> /dev/null; then
    fi
 fi
 
-source recipes/asdf.sh
-source recipes/code.sh
-source recipes/essentials.sh
-source recipes/node.sh
-source recipes/pnpm.sh
+
+shopt -s expand_aliases
+
+function install_asdf {
+   local asdf_folder=$HOME/.asdf
+
+   if [ -d $asdf_folder ]; then
+      gum_log warn "asdf already installed. skipping"
+   else 
+      local asdf_version=$(curl -s https://api.github.com/repos/asdf-vm/asdf/releases | jq -r '.[0].tag_name')
+      git config --global advice.detachedHead false
+      _ git clone https://github.com/asdf-vm/asdf.git ~/.asdf --single-branch --branch $asdf_version
+
+      echo 'source $HOME/.asdf/asdf.sh' >> $HOME/.bashrc
+      echo 'source $HOME/.asdf/completions/asdf.bash' >> $HOME/.bashrc
+   fi
+}
+shopt -s expand_aliases
+
+function install_code {
+   if ! command -v code &> /dev/null; then
+      gum_log error "make sure vscode is installed and the code command is added to PATH"
+      exit 1
+   fi
+
+   declare -a extensions;
+
+   extensions+=( "naumovs.color-highlight" )
+   extensions+=( "dracula-theme.theme-dracula" )
+   extensions+=( "DaltonMenezes.aura-theme" )
+   extensions+=( "pkief.material-icon-theme" )
+   extensions+=( "styled-components.vscode-styled-components" )
+   extensions+=( "wix.vscode-import-cost" )
+   extensions+=( "streetsidesoftware.code-spell-checker" )
+   extensions+=( "streetsidesoftware.code-spell-checker-portuguese-brazilian" )
+   extensions+=( "eamodio.gitlens" )
+   extensions+=( "moyu.snapcode" )
+   extensions+=( "formulahendry.auto-rename-tag" )
+   extensions+=( "dbaeumer.vscode-eslint" )
+   extensions+=( "yoavbls.pretty-ts-errors" )
+   extensions+=( "miguelsolorio.fluent-icons" )
+   extensions+=( "drcika.apc-extension" )
+   extensions+=( "rust-lang.rust-analyzer" )
+
+   installed_extensions=$(code --list-extensions)
+
+   for ext in "${extensions[@]}"; do
+      echo "${installed_extensions[@]}" | grep -iq $ext
+      already_installed=$?
+
+      if [[ $already_installed -ne 0 ]]; then
+         gum_log info "installing $ext"
+         code --install-extension $ext
+      else
+         gum_log warn "code extension $ext already installed. skipping"
+      fi
+   done
+}
+shopt -s expand_aliases
+
+function install_essentials {
+   packages=(
+      "xsel"
+      "rg"
+      "neofetch"
+      "tmux"
+      "zoxide"
+      "jq"
+      "gh"
+      "glow"
+      "fzf"
+      "gpg"
+   )
+
+   for pkg in "${packages[@]}"; do
+      if ! command -v $pkg &> /dev/null; then
+         gum_log info "installing $pkg"
+         _ brew install $pkg
+         if [ $? -ne 0 ]; then
+            gum_log error "failed to install $pkg"
+         fi
+      else
+         gum_log warn "$pkg already installed. skipping"
+      fi
+   done
+}
+shopt -s expand_aliases
+
+function install_node {
+   _ asdf plugin add nodejs
+   _ asdf install nodejs 18.20.2
+   _ asdf global nodejs 18.20.2
+}
+shopt -s expand_aliases
+
+function install_pnpm {
+   if ! command -v pnpm &> /dev/null; then
+      _ asdf plugin-add pnpm
+      _ asdf install pnpm latest
+      _ asdf global pnpm latest
+   else 
+      gum_log warn "pnpm already installed. skipping"
+   fi
+}
 
 declare -A options;
 for opt in $@; do 
