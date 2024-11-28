c265ef53ebbeb163e40a672ac5fee0c997db8a9a
pmqueiroz <pedroqueiroz593@gmail.com>
2024-11-27 23:24:41 -0300

    feat: better checkout

--- dots/aliases.sh
+++ dots/aliases.sh
@@ -23,8 +23,12 @@ function commit_title {
    echo "$1" | awk '{gsub("/",": "); print}' | awk '{gsub("-"," "); print}'
 }
 
-function git_style {
-  gum style "$(gum style --foreground "#f14e32" '') $(gum style --bold --underline --foreground "#f14e32" "$@")"
+function git_branch_style {
+  gum style "🌿 $(gum style --bold --underline --foreground "#BD93F9" "$@")"
+}
+
+function git_pr_style {
+  gum style "$(gum style --foreground "#FF79C6" '#')$(gum style --bold --underline --foreground "#BD93F9" "$@")"
 }
 
 function fpush {
@@ -34,7 +38,7 @@ function fpush {
    fi
 
    branch=$(git branch --show-current)
-   gum_log info "pushing and setting upstream to $(git_style "$branch")"
+   gum_log info "pushing and setting upstream to $(git_branch_style "$branch")"
 
    git push "$1" --set-upstream origin "$branch" "$2"
 }
@@ -67,7 +71,7 @@ function commit {
       commitmsg="$inputted_message"
    fi
 
-   gum_log info "commiting files with message $(git_style "$commitmsg")"
+   gum_log info "commiting files with message $(git_branch_style "$commitmsg")"
 
    gum style --foreground 12 --margin "0 1" -- "$(git diff --name-only --cached)"
 
@@ -82,11 +86,10 @@ function get_branch_by_number {
     local repo_name
     local branch_name
 
-    repo_name=$(gh repo view --json name --jq '.name')
-    branch_name=$(gh pr view "$pr_number" --json headRefName --repo "$repo_name" --jq '.headRefName')
+    repo_name=$(gh repo view --json nameWithOwner --jq '.nameWithOwner')
+    branch_name=$(gh pr view "$pr_number" --json headRefName --repo "$repo_name" --jq '.headRefName' 2>/dev/null)
 
     if [ $? -ne 0 ]; then
-        echo "Error retrieving branch name for PR #$pr_number"
         return 1
     fi
 
@@ -105,9 +108,11 @@ function checkout {
       branch_to_checkout=$(git symbolic-ref refs/remotes/origin/HEAD | sed 's@^refs/remotes/origin/@@')
    else
       if [[ $1 =~ ^[0-9]+$ ]]; then
-         branch_by_number=$(get_branch_by_number "$1")
+         gum_log info "fetching branch to $(git_pr_style "$1")"
+          branch_by_number=$(get_branch_by_number "$1")
 
          if [ $? -ne 0 ]; then
+            gum_log error "could not determine branch to $(git_pr_style "$1")"
             return 1
          fi
 
@@ -118,20 +123,20 @@ function checkout {
    fi
 
    if [ "$current_branch" != "$branch_to_checkout" ]; then
-      gum_log info "checking out to $(git_style "$branch_to_checkout")"
+      gum_log info "checking out to $(git_branch_style "$branch_to_checkout")"
       git checkout --quiet "$branch_to_checkout"
    fi
 
-   gum_log info 'updating branch'
+   gum_log info "updating current branch"
 
    git pull --rebase --quiet
 
-   local_branchs=$(git for-each-ref --format='%(refname:short)' refs/heads/ | sed /^"$branch_to_checkout"\$/d)
+   local_branchs=$(git for-each-ref --format='%(refname:short)' refs/heads/ | grep -v "^$branch_to_checkout$")
 
    if test -n "$local_branchs"; then
       gum_log info 'deleting other branches. was:'
       for branch in "${local_branchs[@]}"; do 
-         git_style "$branch"
+         git_branch_style "$branch"
       done
 
       echo "$local_branchs" | xargs git branch -q -D
