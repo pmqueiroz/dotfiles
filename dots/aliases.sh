a5ffd3884931dfa6adc2a5c0c83d7af67496fb61
pmqueiroz <pedroqueiroz593@gmail.com>
2024-11-27 22:34:52 -0300

    feat: checkout using pr number

--- dots/aliases.sh
+++ dots/aliases.sh
@@ -12,7 +12,7 @@ function pr {
 
    if [ $? -ne 0 ]; then
       title=$(commit_title $(git branch --show-current))
-      gh pr create --assignee @me --title "$title" --web $@
+      gh pr create --assignee @me --title "$title" --web "$@"
       return 
    fi
 
@@ -20,7 +20,7 @@ function pr {
 }
 
 function commit_title {
-   echo $1 | awk '{gsub("/",": "); print}' | awk '{gsub("-"," "); print}'
+   echo "$1" | awk '{gsub("/",": "); print}' | awk '{gsub("-"," "); print}'
 }
 
 function git_style {
@@ -33,10 +33,10 @@ function fpush {
       return 128
    fi
 
-   branch=`git branch --show-current`
-   gum_log info "pushing and setting upstream to $(git_style $branch)"
+   branch=$(git branch --show-current)
+   gum_log info "pushing and setting upstream to $(git_style "$branch")"
 
-   git push $1 --set-upstream origin $branch $2
+   git push "$1" --set-upstream origin "$branch" "$2"
 }
 
 function commit {
@@ -47,7 +47,7 @@ function commit {
    declare -a commit_options;
    declare -a inputted_message;
 
-   for arg in $@; do
+   for arg in "$@"; do
       if [[ $arg == -* ]]; then
          commit_options+=( "$arg" )
       else
@@ -62,7 +62,7 @@ function commit {
 
    if test -z "$inputted_message"
    then
-      commitmsg=`commit_title $(git branch --show-current)`
+      commitmsg=$(commit_title "$(git branch --show-current)")
    else
       commitmsg="$inputted_message"
    fi
@@ -71,10 +71,26 @@ function commit {
 
    gum style --foreground 12 --margin "0 1" -- "$(git diff --name-only --cached)"
 
-   gum confirm "confirm?" && git commit -m "${commitmsg}" ${commit_options[@]} || {
+   gum confirm "confirm?" && git commit -m "${commitmsg}" "${commit_options[@]}" || {
       gum_log error exiting...
       git reset
    }
+}
+
+function get_branch_by_number {
+    local pr_number=$1
+    local repo_name
+    local branch_name
+
+    repo_name=$(gh repo view --json name --jq '.name')
+    branch_name=$(gh pr view "$pr_number" --json headRefName --repo "$repo_name" --jq '.headRefName')
+
+    if [ $? -ne 0 ]; then
+        echo "Error retrieving branch name for PR #$pr_number"
+        return 1
+    fi
+
+    echo "$branch_name"
 }
 
 function checkout {
@@ -88,40 +104,50 @@ function checkout {
    if test -z "$1"; then
       branch_to_checkout=$(git symbolic-ref refs/remotes/origin/HEAD | sed 's@^refs/remotes/origin/@@')
    else
-      branch_to_checkout="$1"
+      if [[ $1 =~ ^[0-9]+$ ]]; then
+         branch_by_number=$(get_branch_by_number "$1")
+
+         if [ $? -ne 0 ]; then
+            return 1
+         fi
+
+         branch_to_checkout="$branch_by_number"
+      else
+         branch_to_checkout="$1"
+      fi
    fi
 
-   if [ $current_branch != $branch_to_checkout ]; then
-      gum_log info "checking out to $(git_style $branch_to_checkout)"
-      git checkout --quiet $branch_to_checkout
+   if [ "$current_branch" != "$branch_to_checkout" ]; then
+      gum_log info "checking out to $(git_style "$branch_to_checkout")"
+      git checkout --quiet "$branch_to_checkout"
    fi
 
    gum_log info 'updating branch'
 
    git pull --rebase --quiet
 
-   local_branchs=$(git for-each-ref --format='%(refname:short)' refs/heads/ | sed /^$branch_to_checkout\$/d)
+   local_branchs=$(git for-each-ref --format='%(refname:short)' refs/heads/ | sed /^"$branch_to_checkout"\$/d)
 
    if test -n "$local_branchs"; then
       gum_log info 'deleting other branches. was:'
-      for branch in ${local_branchs[@]}; do 
-         git_style $branch
+      for branch in "${local_branchs[@]}"; do 
+         git_style "$branch"
       done
 
-      echo $local_branchs | xargs git branch -q -D
+      echo "$local_branchs" | xargs git branch -q -D
    fi
 }
 
 function run_node {
    tmp_file=$(mktemp --suffix=.js)
-   code $tmp_file --wait
-   file_content=$(cat ${tmp_file})
+   code "$tmp_file" --wait
+   file_content=$(cat "${tmp_file}")
    node -p -e "${file_content}"
-   rm -rf $tmp_file
+   rm -rf "$tmp_file"
 }
 
 function checksum {
-   md5sum $1 | awk '{ print $1 }'
+   md5sum "$1" | awk '{ print $1 }'
 }
 
 function hot {
@@ -133,8 +159,8 @@ function hot {
    a='';
 
    while true; do
-      b=`ls -l $*`
-      [[ $a != $b ]] && a=$b && eval $script;
+      b=$(ls -l "$*")
+      [[ $a != $b ]] && a=$b && eval "$script";
       sleep .5;
    done
   fi
