6a6d4996d70a6cb0becf5c9cab8818cb8a9600e7
pmqueiroz <pedroqueiroz593@gmail.com>
2025-08-07 15:58:02 -0300

    fix: remove completition files

--- recipes/asdf.sh
+++ recipes/asdf.sh
@@ -1,18 +1,17 @@
 #!/usr/bin/env bash
 
-shopt -s expand_aliases
+shopt -s expand_aliases 
 
 function install_asdf {
    local asdf_folder=$HOME/.asdf
 
-   if [ -d $asdf_folder ]; then
+   if [ -d "$asdf_folder" ]; then
       gum_log warn "asdf already installed. skipping"
    else 
       local asdf_version=$(curl -s https://api.github.com/repos/asdf-vm/asdf/releases | jq -r '.[0].tag_name')
       git config --global advice.detachedHead false
-      _ git clone https://github.com/asdf-vm/asdf.git ~/.asdf --single-branch --branch $asdf_version
+      _ git clone https://github.com/asdf-vm/asdf.git ~/.asdf --single-branch --branch "$asdf_version"
 
-      echo 'source $HOME/.asdf/asdf.sh' >> $HOME/.bashrc
-      echo 'source $HOME/.asdf/completions/asdf.bash' >> $HOME/.bashrc
+      echo 'source $HOME/.asdf/asdf.sh' >> "$HOME"/.bashrc
    fi
 }
